620019395494efdabe87a8e5df6898e41102b30e
kimtaekjin <qlcdmlrlf@naver.com>
2024-05-07 13:16:38 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Login from './components/sideEffect/Login/Login';
 import MainHeader from './components/sideEffect/MainHeader/MainHeader';
 import Home from './components/sideEffect/Home/Home';
@@ -10,10 +10,23 @@ const App = () => {
 
   //화면이 리렌더링 될 때 localStorage를 확인해서
   //현재 login-flag가 존재하는지 검사
-  const storedLoginFlag = localStorage.getItem('login-flag');
-  if (storedLoginFlag === '1') {
-    setisLoggedIn(true);
-  }
+  // const storedLoginFlag = localStorage.getItem('login-flag');
+  // if (storedLoginFlag === '1') {
+  //   setisLoggedIn(true);
+  // }
+
+  //기존에 로그인 한 사람인지 확인하는 코드는
+  //리렌더링 될 때마다 실행하면 안됨(한번만 확인하면 됨-무한루프 위험성)
+  useEffect(() => {
+    console.log('useEffect 실행 -최초 단 한번만 실행');
+    const storagedLoginFlag = localStorage.getItem('login-flag');
+    if (storagedLoginFlag === '1') {
+      setisLoggedIn(true);
+    }
+  }, []);
+  //의존성 배열: useEffect가 실행되어야 하는 트리거 변수.
+  //배열 안에 변수를 지정하면, 해당 변수의 값이 변할 때마다 useEffect가 실행됨.
+  //만약 배열을 비워놓으면,렌더링 과정에서 단 한번만 실행됨.
 
   //서버로 로그인을 요청하는 함수.(나중에는 fetch를 통한 백엔드와의
   //연계가 필요합니다.)
@@ -24,15 +37,21 @@ const App = () => {
     setisLoggedIn(true);
   };
 
-  return (
-    <>
-      <MainHeader />
-      <main>
-        {isLoggedIn && <Home />}
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-      </main>
-    </>
-  );
-};
+  const LogoutHandler = () => {
+    localStorage.removeItem('login-flag');
+    setisLoggedIn(false);
+  };
 
+  const MainHeadler = ({ isLoggedIn }, { LogoutHandler }) => {
+    return (
+      <>
+        <MainHeader />
+        <main>
+          {isLoggedIn && <Home />}
+          {!isLoggedIn && <Login onLogin={loginHandler} />}
+        </main>
+      </>
+    );
+  };
+};
 export default App;
